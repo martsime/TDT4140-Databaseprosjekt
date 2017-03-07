e11e5f7342869603520afdf0b00429c63aace2de
Lemonous <Lemonous@users.noreply.github.com>
2017-03-07 09:56:05 +0100

    Add statistics view and controller

---
+++ src/statistics/Controller.java
@@ -0,0 +1,12 @@
+package statistics;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class Controller implements PropertyChangeListener {
+
+    @Override
+    public void propertyChange(PropertyChangeEvent event) {
+
+    }
+}
